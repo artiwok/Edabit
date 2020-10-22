106c888b6fbbe53190f11fdd253e2e33e1b23d4a
artiwok <artemolga@ukr.net>
2020-10-22 14:59:12 +0300

    Add project files.

---
+++ NumberSquared/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberSquared
+{
+    class Program
+    {
+        static int NumSqr(int num)
+        {
+            int sum = 0;
+            int step = 1;
+            for (int i = 0; i < num; i++)
+            {
+                sum += step;
+                step += 2;
+            }
+
+            Console.WriteLine(sum);
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("Number squared is: ");
+            NumSqr(num);
+        }
+    }
+}
